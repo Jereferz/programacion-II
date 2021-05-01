6fa998f66e1ee1653d64f15475ebc22a439e4a9b
Jereferz <jere.fer1999@gmail.com>
2021-05-01 20:35:25 -0300

    segunda semana agregada

---
+++ Semana2/src/paque1/ciudadano.java
@@ -0,0 +1,22 @@
+package paque1;
+
+public class ciudadano {
+		
+	private String Provi; 
+	private long DNI;
+	public String getProvincia() {
+		return Provi;
+	}
+	
+	public void setProvincia(String Provi) {
+		this.Provi = Provi;
+	}
+	
+	public long getDni() {
+		return DNI;
+	}
+	
+	public void setDni(long DNI) {
+		this.DNI = DNI;	
+	}
+}
---
+++ Semana2/src/paque1/persona.java
@@ -0,0 +1,23 @@
+package paque1;
+
+
+public class persona {
+	
+	private String Apellido;
+	private String Nombre;
+	public String getApeellido() {
+		return Apellido;
+	}
+	
+	public void setApeellido(String Apellido) {
+		this.Apellido = Apellido;
+	}
+	
+	public String getNombre() {
+		return Nombre;
+	}
+	
+	public void setNombre(String Nombre) {
+		this.Nombre = Nombre;
+	}
+}
